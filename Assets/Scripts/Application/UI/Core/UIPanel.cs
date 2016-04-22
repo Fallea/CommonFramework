a67c7c8ea86f86054b74cb00fc7bc0950b7ce1c2
Fallea <214439700@qq.com>
2016-04-22 17:33:24 +0800

    Add UI code files.

---
+++ Assets/Scripts/Application/UI/Core/UIPanel.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*******************************************************************************
+ * 
+ *             类名: UIPanel
+ *             功能: UI面板类
+ *             作者: HGQ
+ *             日期: 2016.4.22
+ *             修改:
+ *             
+ * *****************************************************************************/
+
+public class UIPanel : MonoBehaviour
+{
+
+    protected virtual void Start()
+    {
+
+    }
+
+    public virtual void Open()
+    {
+
+    }
+
+    public virtual void Open(object param)
+    {
+
+    }
+
+    public virtual void Close()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Application/UI/Core/UIViewItem.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*******************************************************************************
+ * 
+ *             类名: UIViewItem
+ *             功能: UI显示项
+ *             作者: HGQ
+ *             日期: 2016.4.22
+ *             修改:
+ *             
+ * *****************************************************************************/
+
+public class UIViewItem : MonoBehaviour
+{
+
+    public delegate void ViewItemDelegate(UIViewItem item);
+
+    private string mId;
+    private int mIndex;
+    private object mData;
+    private bool mSeleted;
+
+    protected bool started;
+
+    /// <summary>
+    /// 字符串ID，用于存储字符串标示
+    /// </summary>
+    public string id
+    {
+        get { return this.mId; }
+        set { this.mId = value; }
+    }
+
+    /// <summary>
+    /// int索引，用于存储int标示
+    /// </summary>
+    public int index
+    {
+        get { return this.mIndex; }
+        set { this.mIndex = value; }
+    }
+
+    public object data
+    {
+        get { return this.mData; }
+        set { this.mData = value; this.UpdateData(); }
+    }
+
+    public bool selected
+    {
+        get { return this.mSeleted; }
+        set { this.mSeleted = value; this.UpdateSelected(); }
+    }
+
+    //================================================================
+
+    protected virtual void DoStart()
+    {
+        //do something...
+    }
+
+    protected virtual void UpdateData()
+    {
+        if (!this.started) { return; }
+        if (this.data == null) { return; }
+        //do something...
+    }
+
+    protected virtual void UpdateSelected()
+    {
+        //do something...
+    }
+
+    //================================================================
+
+    protected virtual void Start()
+    {
+        this.DoStart();
+        this.started = true;
+        this.UpdateData();
+        this.UpdateSelected();
+    }
+
+    //================================================================
+
+    public virtual void Clear()
+    {
+        this.mData = null;
+    }
+}
